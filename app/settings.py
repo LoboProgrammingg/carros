7cc7921684aff3228c0840b950378b7841e86895
loboprogrammingg <matheusloboo2001@gmail.com>
2024-07-19 21:06:23 -0400

    m

--- app/settings.py
+++ app/settings.py
@@ -156,5 +156,5 @@ logging.basicConfig(
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
