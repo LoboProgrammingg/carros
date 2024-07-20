fa4e3d2a4375e807c65b99aea76b011a81754f2c
loboprogrammingg <matheusloboo2001@gmail.com>
2024-07-19 20:35:26 -0400

    config uwsgi_params and static_root env config

--- app/settings.py
+++ app/settings.py
@@ -141,10 +141,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Configurações dos arquivos estáticos
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 import logging
 
