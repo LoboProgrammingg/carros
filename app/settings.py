f7848440c3bc6e09f0aaf295cfad5ba202be6905
loboprogrammingg <matheusloboo2001@gmail.com>
2024-07-22 15:52:33 -0400

    l

--- app/settings.py
+++ app/settings.py
@@ -141,7 +141,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Configurações dos arquivos estáticos
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 import logging
