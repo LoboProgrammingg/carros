36c6b01a77db0e7d76f9830d638b694efeb1b123
matheus lobo <klebercamara12@gmail.com>
2025-02-06 08:10:22 -0400

    l

--- app/settings.py
+++ app/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carros',
-        'USER': 'postgres',
-        'PASSWORD': 'sarutobi12',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
